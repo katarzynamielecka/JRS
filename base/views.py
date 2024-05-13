a4c34818b15f661c9382c603d9ec51a3b1f84c45
kmieleck <01168899@pw.edu.pl>
2024-05-13 23:09:58 +0200

    env

--- base/views.py
+++ base/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Test
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        input_text = request.POST.get('inputField')
+        Test.objects.create(text=input_text) 
+    all_data = Test.objects.all()
+    return render(request, 'home.html', {'all_data': all_data})
